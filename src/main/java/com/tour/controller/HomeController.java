ed003ed96a0cd02cddd7e3b2d390e88ce1f7cc33
livingroom-pc\John <scurrymode@gmail.com>
2017-06-22 17:07:42 +0900

    main page css 적용

--- src/main/java/com/tour/controller/HomeController.java
+++ src/main/java/com/tour/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.tour.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,7 +10,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	@RequestMapping("/main")
-	public String home() {
+	public String main_page(Model model){
+		model.addAttribute("main_jsp", "/bus");
 		return "main/main";
+	}
+	@RequestMapping("/bus")
+	public String bus_page(){
+		return "main/bus";
+	}
+	
+	@RequestMapping("/main1")
+	public String main1_page(){
+		return "main/main1";
+	}
+	@RequestMapping("/content")
+	public String main_content_section(){
+		return "main/content";
 	}
 }

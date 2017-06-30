f4e5b101e85dac9979a6503c99dd579e1868bcfb
sist117\sist <seize810@gmail.com>
2017-06-30 15:16:47 +0900

    TimeSchedule sysout 제거

--- src/main/java/com/tour/dao/TimeScheduleDAO.java
+++ src/main/java/com/tour/dao/TimeScheduleDAO.java
@@ -20,19 +20,9 @@ public class TimeScheduleDAO {
 	
 	
 	public List<ScheduleDayVO> tourDateList(int tour_id){
-		System.out.println("TimeScheduleDAO.tourDateList");
+		//System.out.println("TimeScheduleDAO.tourDateList");
 		List<ScheduleDayVO> list=tsMapper.tourDateList(tour_id);
-		System.out.println(list.get(0).getPlaceVO().size());
-		for(int i=0;i<list.get(0).getCityVO().size();i++){
-			System.out.println(i+" : "+list.get(0).getCityVO().get(i).getName());
-		}
-		/*for (int i=0;i<list.size();i++){
-			System.out.println(list.get(i).getTour_date());
-			
-			list.get(i).setCityVO(tsMapper.tourDateCityList(tour_id, list.get(i).getTour_date()));
-			//list.get(i).setPlaceVO(tsMapper.tourDatePlaceList(tour_id, list.get(i).getTour_date()));
-		}*/
-		
+	
 		return list;
 	}
 
